94e2f1b394b57b28bd989475aab304de9f6b5f53
sana-sagegami <sana01237@icloud.com>
2025-10-21 13:06:38 +0900

    Initial commit: ShareNight app setup

---
+++ ShareNight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShareNight
+//
+//  Created by x24042xx on 2025/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShareNight/ShareNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareNightApp.swift
+//  ShareNight
+//
+//  Created by x24042xx on 2025/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ShareNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
